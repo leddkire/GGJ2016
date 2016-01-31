0a49a5737077e7aa38fbe010d249d90294f890db
fgvzla <gamundiaranda@gmail.com>
2016-01-31 15:17:18 -0430

    the end

---
+++ game/the_end/the_end.gd
@@ -0,0 +1,12 @@
+
+extends Node2D
+
+func _input(ev):
+	if(ev.is_action_pressed("repulsor")):
+		get_node("/root/game_data")._reset_items()
+		var last_scn = get_node("/root/global").last_scene_path
+		get_node("/root/global").goto_playable_scene(last_scn,null)
+
+func _ready():
+	set_process_input(true)
+
