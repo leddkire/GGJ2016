055a1d4fc2e17a411f9ab57f8426d11170e37a30
leddkire <leddkire@gmail.com>
2016-02-03 15:47:27 -0430

    Fixed wolf behaviour and revamped states

--- game/wolf_enemy/wolf_enemy.gd
+++ game/wolf_enemy/wolf_enemy.gd
@@ -1,19 +1,109 @@
 
 extends KinematicBody2D
 
-const player_class = preload("res://player.gd")
+
+var StateObject = load("res://classes/StateObject.gd").StateObject
 
 const WALK_SPEED = 700
 const RUN_SPEED = 1400
-const JUMP_SPEED = 500
-const JUMP_HEIGHT = 650
+const JUMP_SPEED = 400
+const JUMP_HEIGHT = 200
 const TIMEOUT_IDLE = 1
 const TIMEOUT_CHARGING = 0.40
 const TIMEOUT_FREEZED = 2 
 const TIMEOUT_ATTACKING = 1
 
+var WANDERING_MS = StateObject.new("wandering","wandering_ms",1,self)
+func wandering_ms():
+	var force = grav_force
+	if(sign(dir_facing)==-1):
+		if(velocity.x > -WALK_MAX_SPEED * xSpeedFactor):
+			force.x -= WALK_FORCE
+	elif(sign(dir_facing) == 1):
+		if( velocity.x < WALK_MAX_SPEED * xSpeedFactor):
+			force.x += WALK_FORCE
+	return force
+
+#Movement States
+#Suffix MS
+var IDLE_MS = StateObject.new("idle","idle_ms",0,self)
+func idle_ms():
+	velocity.x = 0
+	return grav_force
 
 
+
+var CHASING_MS = StateObject.new("chasing","chasing_ms",2,self)
+func chasing_ms():
+	var chasing_factor = xSpeedFactor * 1.5
+	var force = Vector2(0,GRAVITY*gravityFactor)
+	if(target == null):
+		current_state_ms = IDLE_MS
+		timer.set_wait_time(TIMEOUT_IDLE)
+		timer.start()
+		return force
+	var dir = target.get_global_pos().x - get_pos().x
+	if(sign(dir) == -1):
+		if(velocity.x > -WALK_MAX_SPEED * chasing_factor):
+			force.x -= WALK_FORCE
+			dir_facing = dir
+	elif(sign(dir) == 1):
+		if(velocity.x < WALK_MAX_SPEED * chasing_factor):
+			force.x += WALK_FORCE
+			dir_facing = dir
+	return force
+
+var CHARGING_MS = StateObject.new("charging","charging_ms",3,self)
+func charging_ms():
+	velocity.x = 0
+	return grav_force
+
+var ATTACKING_MS = StateObject.new("attacking","attacking_ms",4,self)
+func attacking_ms():
+	var force = grav_force
+	var vsign = sign(velocity.x)
+	var absv = abs(velocity.x)
+	if(absv < 0):
+		velocity.x = 0
+	else:
+		force.x += - vsign * STOP_FORCE
+	return force
+
+var STUNNED_MS = StateObject.new("stunned","stunned_ms",5,self)
+func stunned_ms():
+	var force = grav_force
+	var vsign = sign(velocity.x)
+	var absv = abs(velocity.x)
+	if(absv < 0):
+		velocity.x = 0
+	else:
+		force.x += - vsign * STOP_FORCE
+
+	return force
+
+#Movement-specific
+var current_state_ms = IDLE_MS
+var dir_facing = 1
+var siding_left = false
+var velocity = Vector2(0,0)
+var move_spd = 1200
+var target
+var target_near
+var patience
+var check_p
+var stop = false
+
+var GRAVITY =900.0
+var WALK_MAX_SPEED = 50 
+var WALK_FORCE = 1000
+var STOP_FORCE = 350
+
+var gravityFactor = 1
+var xSpeedFactor = 1
+var grav_force = Vector2(0,GRAVITY*gravityFactor)
+var timer
+var gravity
+var cast
 # States
 const IDLE = 1
 const WANDERING = 2
@@ -21,111 +111,105 @@ const CHASING = 3
 const CHARGING = 4
 const ATTACKING = 5
 const FREEZED = 6
+var state
 
-var state = IDLE
-var facing_dir = 1
-var timer
-var velocity = Vector2()
-var gravity
-var target
-var cast
+func process_movement(delta):
+	var new_siding_left = siding_left
+	var force = current_state_ms.stateFunc.call_func()
+	velocity += force*delta
+	var motion = velocity*delta
+	motion = move(motion)
+	if(is_colliding()):
+		var n = get_collision_normal()
+		motion = n.slide(motion)
+		velocity = n.slide(velocity)
+		move(motion)
+	if(sign(dir_facing) == -1):
+		new_siding_left = true
+	else:
+		new_siding_left = false
+	if(new_siding_left != siding_left):
+		if (new_siding_left):
+			set_scale( Vector2(-1,1) )
+			dir_facing = -1
+		else:
+			set_scale( Vector2(1,1) )
+			dir_facing = 1
+		siding_left=new_siding_left
 
+func process_map_collisions():
+	if(is_colliding()):
+		var body = get_collider()
+		var n = get_collision_normal()
+		if(n.x !=0):
+			dir_facing = n.x
+			
+func _fixed_process(delta):
+	process_map_collisions()
+	process_movement(delta)
+	get_node("Label").set_text(current_state_ms.name)
+	get_node("Label").set_scale(Vector2(sign(dir_facing),1))
+
+#Signal functions
 func _on_contact_receiver_body_enter(body):
 	if(body.is_in_group("repulsor")):
-		print("REPUL/SED")
-		velocity.y=-500
+		velocity.y=-250
 		var rep_x = body.get_global_pos().x
 		var diff_x = get_pos().x - rep_x
-		velocity.x = sign(diff_x) * 1000
-		state = FREEZED
+		velocity.x = sign(diff_x) * 300
+		current_state_ms = STUNNED_MS
 		timer.set_wait_time(TIMEOUT_FREEZED)
 		timer.start()
 	if(body.is_in_group("projectile")):
-		print("SHOT")
 		get_node("/root/game_data").add_item_qty("tooth.png")
 		body.queue_free()
 		queue_free()
 
+
 func _on_attack_range_body_enter(body):
-	if(body.is_in_group("player") and (state == IDLE or state == WANDERING or state == CHASING)):
-		state = CHARGING
+	if(body.is_in_group("player") and (current_state_ms == IDLE_MS or current_state_ms == WANDERING_MS or current_state_ms == CHASING_MS) and not current_state_ms == STUNNED_MS):
+		current_state_ms = CHARGING_MS
 		if (body.get_pos().x > get_pos().x):
-			facing_dir = 1
+			dir_facing = 1
 		else:
-			facing_dir = -1
+			dir_facing = -1
 		timer.set_wait_time(TIMEOUT_CHARGING);
 		timer.start()
 		target = body
 
 
 func _on_Timer_timeout():
-	if state == CHARGING:
+	if current_state_ms == CHARGING_MS:
 		velocity.y = -JUMP_HEIGHT
-		velocity.x = facing_dir * JUMP_SPEED
-		state = ATTACKING
+		velocity.x = dir_facing * JUMP_SPEED
+		current_state_ms = ATTACKING_MS
 		timer.set_wait_time(TIMEOUT_ATTACKING)
 		timer.start()
-	elif state == IDLE:
+	elif current_state_ms == IDLE_MS:
 		randomize();
 		var ran = randi() % 2
 		if ran == 0:
-			facing_dir = -1
+			dir_facing = -1
 		else:
-			facing_dir = 1
-		state = WANDERING
+			dir_facing = 1
+		current_state_ms = WANDERING_MS
 		randomize();
 		ran = randi() % 2
 		timer.set_wait_time(ran + 2)
 		timer.start()
-	elif state == FREEZED:
-		state = WANDERING
-	elif state == ATTACKING:
-		state = WANDERING
-	elif state == WANDERING:
-		state = IDLE
+	elif current_state_ms == STUNNED_MS:
+		current_state_ms = WANDERING_MS
+	elif current_state_ms == ATTACKING_MS:
+		current_state_ms = WANDERING_MS
+	elif current_state_ms == WANDERING_MS:
+		current_state_ms = IDLE_MS
 		timer.set_wait_time(TIMEOUT_IDLE)
 		timer.start()
 
-func perform_wolf_movement(delta):
-	velocity.y += 100
-	if state == WANDERING:
-		velocity.x = facing_dir * WALK_SPEED
-	elif state == CHASING:
-		var pos = get_pos()
-		var target_pos = target.get_pos()
-		var pos_diff = target_pos - pos
-		velocity.x = RUN_SPEED * sign(pos_diff.x)
-		facing_dir = sign(pos_diff.x)
-	elif state == IDLE or state == CHARGING:
-		velocity.x = 0
-	elif (state == FREEZED):
-		pass
-	var motion = velocity* delta
-	motion = move(motion)
-	if(is_colliding()):
-		var norm = get_collision_normal()
-		motion = norm.slide(motion)
-		velocity = norm.slide(velocity)
-
-func check_facing():
-	var scale = get_scale()
-	if(sign(facing_dir) == 1):
-		set_scale(Vector2(1,scale.y))
-	else:
-		set_scale(Vector2(-1,scale.y))
-
-func process_map_collisions():
-	if(is_colliding()):
-		var body = get_collider()
-		if(body.is_in_group("wall")):
-			facing_dir = -facing_dir
-			
-
-func _fixed_process(delta):
-	process_map_collisions()
-	perform_wolf_movement(delta)
-	check_facing()
-
+func _on_cast_range_body_enter( body ):
+	if(body.is_in_group("player") and not (current_state_ms == ATTACKING_MS or current_state_ms == CHARGING_MS or current_state_ms == STUNNED_MS)):
+		current_state_ms = CHASING_MS
+		target = body
 
 
 func _ready():
@@ -140,7 +224,4 @@ func _ready():
 
 
 
-func _on_cast_range_body_enter( body ):
-	if(body.is_in_group("player") and not (state == ATTACKING or state == CHARGING)):
-		state = CHASING
-		target = body
+
